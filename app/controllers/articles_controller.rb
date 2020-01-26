b1e5459320f2886e5c9152731d116dae6f2a0793
Thanh <nuhoangchenhac@gmail.com>
2020-01-26 09:52:26 +0700

    Edit Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,10 +14,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Articles was sucessfully update"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   private
   def article_params
     params.require(:article).permit(:title,:description)
