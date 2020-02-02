8fb80bbdbca20a13803817fe8a1410f52307892b
Thanh <nuhoangchenhac@gmail.com>
2020-02-02 09:45:16 +0700

    user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_133707) do
+ActiveRecord::Schema.define(version: 2020_02_01_140043) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_31_133707) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
