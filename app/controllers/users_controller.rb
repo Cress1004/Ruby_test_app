0b8463d1990e3629459c5ff0a92963961a837abc
Thanh <nuhoangchenhac@gmail.com>
2020-02-04 22:36:54 +0700

    add authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def index
       @users = User.all.page(params[:page]).per(5)
   end
@@ -18,11 +19,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if(@user.update(user_params))
       flash[:success]="Your account was updated successfully"
       redirect_to articles_path
@@ -32,12 +31,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.all.page(params[:page]).per(5)
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
