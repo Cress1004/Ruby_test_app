8f18125892058335d50d1784c40d4d7a1f38a963
Thanh <nuhoangchenhac@gmail.com>
2020-02-05 15:29:43 +0700

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
    # render plain: params[:article].inspect # Hien thi ra cac record dang don gian
    #debugger
    @article = Article.new(article_params)
-   @article.user = User.first
+   @article.user = User.current_user
    if @article.save
       flash[:success] = "Article was submitted succsefully"
       redirect_to article_path(@article)
