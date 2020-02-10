abfc7dae5d826dd1b0693ba949d78dc9eec0b179
Thanh <nuhoangchenhac@gmail.com>
2020-02-10 20:25:51 +0700

    Categories form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,23 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.all.page(params[:page]).per(5)
   end
 
   private
